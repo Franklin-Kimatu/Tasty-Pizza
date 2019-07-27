f57e232258ac6a209e5f8ad9d7ce26da5132bdcc
Franklin-Kimatu <frankimatu@gmail.com>
2019-07-27 16:54:23 +0300

    This commit will add functionality to delivery select buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
+
+
     $(".orderbutton").click(function(){
         $(".fourteen1").fadeIn();
     });
     $(".cancel").click(function(){
         $(".fourteen1").fadeut();
-    })
+    });
+
+    $("#doorStepDelivery").click(function(){
+        $("#location").show();
+    });
 });
+
+function getSelectValue(){
+    var selectedSizeValue= document.getElementById("pizzaSize").value;
+    var selectedCrustValue=document.getElementById("crust").value;
+    var selectedToppingValue= document.getElementById("toppings").value;
+    var selectedNumber= document.getElementById("many").value;
+    alert(selectedSizeValue);
+    alert(selectedCrustValue);
+    alert(selectedToppingValue);
+    alert(selectedNumber);
+}
