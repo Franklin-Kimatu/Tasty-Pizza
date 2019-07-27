c243add54828e7c72e6e8551c986a5c4ab48f902
Franklin-Kimatu <frankimatu@gmail.com>
2019-07-27 18:48:38 +0300

    This commit will add more functionality to add another order button

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,54 @@ $(document).ready(function(){
         $(".fourteen1").fadeIn();
     });
     $(".cancel").click(function(){
-        $(".fourteen1").fadeut();
+        $(".fourteen1").hide();
     });
+    $("#ok").click(function(){
+        $(".fourteen1").fadeOut();
+    })
 
     $("#doorStepDelivery").click(function(){
-        $("#location").show();
+        $("#location").fadeIn();
+    });
+
+    $("#addAnotherOrder").click(function(){
+        $("#choices").append('<div class="form-group">' +
+        '<label for="pizzaSize">Choose the size:</label>'+
+        '<select class="form-control" id="pizzaSize" onchange="getSelectValu();">'+
+            '<option value="400">Small -- Ksh. 400</option>'+
+            '<option value="700">Medium -- Ksh. 700</option>'+
+            '<option value="900">Large -- Ksh. 900</option>'+
+        '</select>'+
+        '<label for="crust">Chooose the crust:</label>'+
+        '<select class="form-control" id="crust" onchange="getSelectValu();">'+
+            '<option value="200">Crispy&nbsp;&nbsp;--&nbsp;&nbsp;&nbsp;&nbsp;Ksh.200</option>'+
+            '<option value="300">Stuffed&nbsp;&nbsp;--&nbsp;&nbsp;&nbsp;Ksh.300</option>'+
+            '<option value="100">Glutten-free&nbsp;&nbsp;-- &nbsp;&nbsp;Ksh100</option>'+
+            '<option value="400">Fried Ramen&nbsp;&nbsp;--&nbsp;&nbsp;Ksh.400</option>'+
+            '<option value="0">None</option>'+
+        '</select>'+
+        '<label for="toppings">Choose toppings:</label>'+
+        '<select class="form-control" id="toppings" onchange="getSelectValu();">'+
+            '<option value="100">Pepperoni.--Ksh.100</option>'+
+            '<option value="150">Mushroom--&nbsp;Ksh.150</option>'+
+            '<option value="100">Onions.&nbsp;&nbsp;--&nbsp;&nbsp;&nbsp;&nbsp;Ksh.100</option>'+
+            '<option value="200">Saucage&nbsp;&nbsp;--&nbsp;&nbsp;Ksh.200</option>'+
+            '<option value="0">None</option>'+
+        '</select>'+
+        '<label for="many">How many?</label>'+
+        '<select class="form-control" id="many" onchange="getSelectValu();" required>'+
+            '<option value="1">1</option>'+
+            '<option value="2">2</option>'+
+            '<option value="3">3</option>'+
+            '<option value="4">4</option>'+
+            '<option value="5">5</option>'+
+            '<option value="6">6</option>'+
+            '<option value="7">7</option>'+
+            '<option value="8">8</option>'+
+            '<option value="9">9</option>'+
+            '<option value="10">10</option>'+
+        '</select><br>'+
+    '</div>');
     });
 });
 
@@ -19,11 +62,11 @@ function getSelectValue(){
     var selectedToppingValue= parseInt(document.getElementById("toppings").value);
     var selectedNumber= parseInt(document.getElementById("many").value);
     var selectedLocation= parseInt(document.getElementById("delivery").value);
-    alert(selectedSizeValue);
-    alert(selectedCrustValue);
-    alert(selectedToppingValue);
-    alert(selectedNumber);
-    alert(selectedLocation);
+    console.log(selectedSizeValue);
+    console.log(selectedCrustValue);
+    console.log(selectedToppingValue);
+    console.log(selectedNumber);
+    console.log(selectedLocation);
 
     var totalAmount= (selectedSizeValue + selectedCrustValue + selectedToppingValue) * selectedNumber + selectedLocation;
      alert(totalAmount);
